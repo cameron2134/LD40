601c5a61e747d4d81dbda86e9d9c879061659a15
Cameron <camerondem2134@hotmail.com>
2017-12-02 16:33:34 +0000

    Core mechanics work

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,20 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class EnemyController : Creature
+public class EnemyController : Skeleton
 {
 
     public Vector2 wanderRangeX, wanderRangeY;
 
     private Vector2 moveToPos;
-    private bool isMoving;
+    private bool isMoving, isIdling, hasDecision;
 
-	// Use this for initialization
-	void Start () {
-		
+
+	void Start ()
+	{
+	    isIdling = true;
 	}
-	
-	// Update is called once per frame
+
 	void Update ()
 	{
 	    if (moveToPos == Vector2.zero)
@@ -26,17 +26,123 @@ public class EnemyController : Creature
 	        moveToPos = Utils.RandomVectorInBox(wanderRangeX, wanderRangeY);
 	    }
 
-	    else if (isMoving && new Vector2(transform.position.x, transform.position.y) == moveToPos)
-	    {
-	        isMoving = false;
-	        StartCoroutine(StopAndWait());
-	    }
 
-        if (isMoving)
-	        transform.position = Vector2.MoveTowards(transform.position, moveToPos, moveSpeed * Time.deltaTime);
+        //if (isIdling)
+            Move();
 	}
 
 
+    void FixedUpdate()
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, 10);
+        //Debug.DrawLine(this.gameObject.transform.position, new Vector2(this.transform.position.x + 10, this.transform.position.y), Color.red, 1, false);
+
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+
+            if (hitColliders[i].tag == "Player" || (hitColliders[i].tag == "Skeleton" && hitColliders[i].gameObject != gameObject) || hitColliders[i].tag == "Body Part")
+            {
+               
+                isIdling = false;
+                isMoving = true;
+
+                if (!hasDecision)
+                    MakeDecision(hitColliders[i].gameObject);
+            }
+
+            else
+            {
+                isIdling = true;
+            }
+
+            i++;
+        }
+    }
+
+
+
+    private void MakeDecision(GameObject obj)
+    {
+        var chance = Random.value;
+
+        if (totalBodyParts != 0)
+        {
+            if (totalBodyParts < (MAX_BODY_PARTS / 2) && chance > 0.7)
+            {
+                // Lower chance to go for skeleton
+                chance = Random.value;
+                if (chance > 0.8)
+                {
+                    if (obj.tag == "Skeleton")
+                        moveToPos = obj.transform.position;
+                }
+
+                else
+                {
+                    if (obj.tag == "Body Part")
+                        moveToPos = obj.transform.position;
+                }
+
+            }
+
+            else if ((totalBodyParts >= (MAX_BODY_PARTS / 2) && totalBodyParts != MAX_BODY_PARTS) && chance > 0.3)
+            {
+                chance = Random.value;
+                if (chance > 0.5)
+                {
+                    if (obj.tag == "Skeleton")
+                        moveToPos = obj.transform.position;
+                }
+
+                else
+                {
+                    if (obj.tag == "Body Part")
+                        moveToPos = obj.transform.position;
+                }
+            }
+
+            else if (totalBodyParts == MAX_BODY_PARTS)
+            {
+                // 100% chance t oattack
+                if (obj.tag == "Skeleton" || obj.tag == "Player")
+                {
+                    Debug.Log("finding player " + obj.name);
+                    moveToPos = obj.transform.position;
+                }
+            }
+        }
+
+        else
+        {
+            // Pick up body parts
+            if (obj.tag == "Body Part")
+                moveToPos = obj.transform.position;
+        }
+
+        hasDecision = true;
+    }
+
+
+
+    private void Move()
+    {
+        if (isMoving)
+        {
+            if (new Vector2(transform.position.x, transform.position.y) != moveToPos)
+                transform.position = Vector2.MoveTowards(transform.position, moveToPos, moveSpeed * Time.deltaTime);
+            else
+            {
+                
+                isMoving = false;
+                if (hasDecision)
+                    hasDecision = false;
+                if (isIdling)
+                    StartCoroutine(StopAndWait());
+            }
+            
+        }
+    }
 
 
 
--- Assets/Scripts/Skeleton.cs
+++ Assets/Scripts/Skeleton.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Skeleton : Creature
 {
 
-    private const int MAX_BODY_PARTS = 9;
+    protected const int MAX_BODY_PARTS = 10;
 
     protected int totalBodyParts;
+
+    protected bool canTransferPart;
 
 
 
@@ -34,20 +36,37 @@ public class Skeleton : Creature
     }
 
 
+    public void TransferBodyPart(int num)
+    {
+        totalBodyParts++;
+    }
+
+
     // Transfer body parts on collision
     protected virtual void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == "Body Part" && totalBodyParts < MAX_BODY_PARTS)
         {
-            Debug.Log("Picked up body part");
             totalBodyParts++;
-            GameManager.Instance.OnBodyPartsIncreased(1);
             GameManager.Instance.DestroyObj(col.gameObject);
         }
 
         else if (col.gameObject.tag == "Skeleton")
         {
-            // Detract body part from other skeleton and add to self
+            // Detract body part from other skeleton and add to self - transfer 1 body part per time contacted - wait 0.5f so its balanced
+            if (canTransferPart && totalBodyParts > 0)
+            {
+                totalBodyParts--;
+                col.gameObject.GetComponent<Skeleton>().TransferBodyPart(1);
+                StartCoroutine(TransferCooldown());
+            }
         }
-    } 
+    }
+
+
+    IEnumerator TransferCooldown()
+    {
+        yield return new WaitForSeconds(0.5f);
+        canTransferPart = true;
+    }
 }
